c34ae1f76357c83413fd877748e4fcb774ce80bc
Brandon_Estrada <65381980+BrEstrada@users.noreply.github.com>
2022-02-16 02:22:32 -0700

    stepper lesson complete

--- StepperApp/StepperApp/ViewController.swift
+++ StepperApp/StepperApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var stepperValue: UIStepper!
+    
+    @IBOutlet weak var labelValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        labelValue.text = "\(stepperValue.value)"
+    }
+    
+    @IBAction func changeStepper(_ sender: UIButton) {
+        stepperValue.minimumValue = -10
+        stepperValue.maximumValue = -5
+        stepperValue.stepValue = 0.5
+        stepperValue.isContinuous = true
+        stepperValue.autorepeat = true
+        stepperValue.wraps = true
+    }
 }
 
