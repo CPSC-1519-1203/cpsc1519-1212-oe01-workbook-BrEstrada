5e09e633150df2f420f3cc58744effd5eef7b0e1
Brandon_Estrada <65381980+BrEstrada@users.noreply.github.com>
2022-02-18 01:21:04 -0700

    buttons, switches, and segmented controls completed

--- IBActionApp/IBActionApp/ViewController.swift
+++ IBActionApp/IBActionApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelResult.frame.size.width = 120
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        switch (sender as AnyObject).tag {
+                case 1:
+                    labelResult.text = "Button 1"
+                case 2:
+                    labelResult.text = "Button 2"
+                default:
+                    labelResult.text = "Default"
+                }
+    }
+    
+    @IBAction func buttonRespond(_ sender: UIButton) {
+        switch sender.tag {
+                case 1:
+                    labelResult.text = sender.titleLabel?.text
+                case 2:
+                    labelResult.text = sender.titleLabel?.text
+                default:
+                    labelResult.text = "Default"
+                }
+    }
 }
 
